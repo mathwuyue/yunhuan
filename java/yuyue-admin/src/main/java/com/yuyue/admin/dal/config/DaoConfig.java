2de9a1e4d4fc8ab293c56feb11c091b4f1d9f8c9
ybwhz <ybw_pub@126.com>
2022-06-10 21:24:35 +0800

    Create DaoConfig.java

---
+++ java/yuyue-admin/src/main/java/com/yuyue/admin/dal/config/DaoConfig.java
@@ -0,0 +1,27 @@
+package com.yuyue.admin.dal.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Dao层配置
+ *
+ * @author bowen
+ * @date 2022-03-10
+ */
+@Configuration
+@MapperScan("com.yuyue.admin.dal.dao")
+public class DaoConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
