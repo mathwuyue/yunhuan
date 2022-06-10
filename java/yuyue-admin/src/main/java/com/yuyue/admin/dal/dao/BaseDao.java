2970b95cbeaad3962697055e4c073eb0f0f2dc8d
ybwhz <ybw_pub@126.com>
2022-06-10 21:25:24 +0800

    Create BaseDao.java

---
+++ java/yuyue-admin/src/main/java/com/yuyue/admin/dal/dao/BaseDao.java
@@ -0,0 +1,12 @@
+package com.yuyue.admin.dal.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * Dao基类
+ *
+ * @author bowen
+ * @date 2022-03-10
+ */
+public interface BaseDao<T> extends BaseMapper<T> {
+}
