17e4b9ac7ee51fba14eb8493560201a431292bf8
ybwhz <ybw_pub@126.com>
2022-06-10 21:29:28 +0800

    Create CaseInfoMapper.java

---
+++ java/yuyue-admin/src/main/java/com/yuyue/admin/mapper/CaseInfoMapper.java
@@ -0,0 +1,20 @@
+package com.yuyue.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yuyue.admin.dal.po.CaseInfoPO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author bowen
+ * @since 2021-04-07
+ */
+public interface CaseInfoMapper extends BaseMapper<CaseInfoPO> {
+    }
