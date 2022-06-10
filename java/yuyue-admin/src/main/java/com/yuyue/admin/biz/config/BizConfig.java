a228eee62953b64680e3923e846cf00de815d154
ybwhz <ybw_pub@126.com>
2022-06-10 21:20:12 +0800

    Create BizConfig.java

---
+++ java/yuyue-admin/src/main/java/com/yuyue/admin/biz/config/BizConfig.java
@@ -0,0 +1,13 @@
+package com.yuyue.admin.biz.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Biz层配置
+ *
+ * @author bowen
+ * @date 2022-03-10
+ */
+@Configuration
+public class BizConfig {
+}
